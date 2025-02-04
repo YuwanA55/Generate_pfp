66e57e2352ab77a0bf7cc9660d695f88802530f2
yuwanpay <95163223+YuwanA55@users.noreply.github.com>
2025-02-04 23:58:30 +0700

    Update View5.jsx

--- src/view/View5.jsx
+++ src/view/View5.jsx
@@ -4,7 +4,7 @@ const View5 = () => {
     const [imageSrc, setImageSrc] = useState('');
 
     // URL tetap untuk menampilkan gambar
-    const baseURL = 'https://puasa23.000webhostapp.com/Seilors_avif/';
+    const baseURL = 'https://pfpgen.yuwan.eu/Seilors_avif/';
   
     // Array yang berisi nama-nama file gambar dari '1.png' hingga '53.png'
     const imageNames = Array.from({ length: 502 }, (_, index) => `${index + 1}.avif`);
