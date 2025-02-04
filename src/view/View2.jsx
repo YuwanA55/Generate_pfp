f11a80bd77d7ea3d9d49a7750235427d2290b64f
yuwanpay <95163223+YuwanA55@users.noreply.github.com>
2025-02-04 23:59:36 +0700

    Update View2.jsx

--- src/view/View2.jsx
+++ src/view/View2.jsx
@@ -4,7 +4,7 @@ const View2 = () => {
     const [imageSrc, setImageSrc] = useState('');
 
     // URL tetap untuk menampilkan gambar
-    const baseURL = 'https://puasa23.000webhostapp.com/zkoolday_avif/';
+    const baseURL = 'https://pfpgen.yuwan.eu/zkoolday_avif/';
   
     // Array yang berisi nama-nama file gambar dari '1.png' hingga '53.png'
     const imageNames = Array.from({ length: 125 }, (_, index) => `${index + 1}.avif`);
